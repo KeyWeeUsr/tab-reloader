2294c6365bd575b704c72546cf0cedcde243e85f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2022-01-08 17:43:10 +0100

    [ADD] Add simple single-window reloading

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
+const { "tabs": bt } = browser;
+
+async function reloadTabs(tabs, flags) {
+    const props = {};
+    if (flags.noCache) {
+        props["bypassCache"] = true;
+    }
+
+    const reloading = [];
+    for (const tab of tabs) {
+        reloading.push(bt.reload(tab.id, props));
+    }
+    return Promise.all(reloading);
+}
+
 function normalizeFormData() {
     const defaults = { "noCache": false, "orderReload": false };
     const result = {};
@@ -7,9 +22,32 @@ function normalizeFormData() {
     return Object.assign(defaults, result);
 }
 
-function onFormSubmit(event) {
-    const flags = normalizeFormData(event);
-    console.log(flags);
+function jsonifyError(err) {
+    return Object.getOwnPropertyNames(err).reduce((data, key) => {
+        data[key] = err[key];
+        return data;
+    }, {})
+}
+
+async function onFormSubmit(event) {
+    const submitButton = "reloadSubmit";
+    document.getElementById(submitButton).disabled = true;
+
+    try {
+        const flags = normalizeFormData(event);
+        const queryParams = {};
+
+        if (true /* !flags.allWindows */) {
+            queryParams["currentWindow"] = true;
+        }
+
+        const tabs = await bt.query(queryParams);
+        await reloadTabs(tabs, flags);
+    } catch (err) {
+        console.error(jsonifyError(err));
+    } finally {
+        document.getElementById(submitButton).disabled = false;
+    }
 }
 
 function addListeners() {
