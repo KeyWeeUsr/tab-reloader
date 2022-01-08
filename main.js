832e88dce43baa6e8ef7da8bc12562af71b14acb
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2022-01-08 18:49:10 +0100

    [FIX] Move reloading logic to the bg service

--- main.js
+++ main.js
@@ -1,12 +1,58 @@
+const { "tabs": bt } = browser;
+
 function main() {
     // prevent duplicate loading
     window.hasRun = true;
     browser.runtime.onMessage.addListener(messageHandler);
 }
 
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function reloadTabs(tabs, flags) {
+    const props = {};
+    const { delayMs } = flags;
+    if (flags.noCache) {
+        props["bypassCache"] = true;
+    }
+
+    const reloading = [];
+    for (const tab of tabs) {
+        const reloadPromise = bt.reload(tab.id, props);
+        if (delayMs > 0) {
+            await reloadPromise;
+            await sleep(delayMs);
+            reloading.push(reloadPromise);
+        } else {
+            reloading.push(reloadPromise);
+        }
+    }
+    return Promise.all(reloading);
+}
+
+function jsonifyError(err) {
+    return Object.getOwnPropertyNames(err).reduce((data, key) => {
+        data[key] = err[key];
+        return data;
+    }, {})
+}
 
 async function messageHandler(msg) {
-    console.log(msg);
+    if (msg.command !== "reload") return;
+
+    try {
+        const queryParams = {};
+
+        if (true /* !flags.allWindows */) {
+            queryParams["currentWindow"] = true;
+        }
+
+        const tabs = await bt.query(queryParams);
+        await reloadTabs(tabs, msg.flags);
+    } catch (err) {
+        console.error(jsonifyError(err));
+    }
 }
 
 if (!window.hasRun) {
--- popup.js
+++ popup.js
@@ -1,30 +1,3 @@
-const { "tabs": bt } = browser;
-
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function reloadTabs(tabs, flags) {
-    const props = {};
-    const { delayMs } = flags;
-    if (flags.noCache) {
-        props["bypassCache"] = true;
-    }
-
-    const reloading = [];
-    for (const tab of tabs) {
-        const reloadPromise = bt.reload(tab.id, props);
-        if (delayMs > 0) {
-            await reloadPromise;
-            await sleep(delayMs);
-            reloading.push(reloadPromise);
-        } else {
-            reloading.push(reloadPromise);
-        }
-    }
-    return Promise.all(reloading);
-}
-
 function normalizeFormData() {
     const defaults = { "delayMs": 0, "noCache": false, "orderReload": false };
     const result = {};
@@ -34,32 +7,12 @@ function normalizeFormData() {
     return Object.assign(defaults, result);
 }
 
-function jsonifyError(err) {
-    return Object.getOwnPropertyNames(err).reduce((data, key) => {
-        data[key] = err[key];
-        return data;
-    }, {})
-}
-
 async function onFormSubmit(event) {
-    const submitButton = "reloadSubmit";
-    document.getElementById(submitButton).disabled = true;
-
-    try {
-        const flags = normalizeFormData(event);
-        const queryParams = {};
-
-        if (true /* !flags.allWindows */) {
-            queryParams["currentWindow"] = true;
-        }
-
-        const tabs = await bt.query(queryParams);
-        await reloadTabs(tabs, flags);
-    } catch (err) {
-        console.error(jsonifyError(err));
-    } finally {
-        document.getElementById(submitButton).disabled = false;
-    }
+    const flags = normalizeFormData(event);
+    await browser.runtime.sendMessage({
+        "command": "reload",
+        "flags": flags
+    });
 }
 
 function addListeners() {
