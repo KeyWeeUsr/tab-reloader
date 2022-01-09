9bbf9c3a3324996fa7d76a6bf283e33476cb5b9a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2022-01-09 13:52:52 +0100

    [ADD] Filter tabs by new scenarios

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const { "tabs": bt } = browser;
+const SC_EVERYTHING = "all";
+const SC_BEFORE_CURRENT_TAB = "before";
+const SC_AFTER_CURRENT_TAB = "after";
+const SC_ATTENTION = "attention";
+const SC_AUDIO = "audio";
+const SC_HIDDEN = "hidden";
+const SC_ARTICLE = "article";
+const SC_READER = "reader";
+const SC_MUTE = "mute";
+const SC_PIN = "pin";
 
 function main() {
     // prevent duplicate loading
@@ -64,24 +74,90 @@ function jsonifyError(err) {
     }, {})
 }
 
+function handleScenario(data, name) {
+    switch (name) {
+    case SC_AUDIO:
+        data["audible"] = true;
+        break;
+    case SC_HIDDEN:
+        data["hidden"] = true;
+        break;
+    case SC_MUTE:
+        data["muted"] = true;
+        break;
+    case SC_PIN:
+        data["pinned"] = true;
+        break;
+    }
+}
+
+function filterTabsByScenario(tabs, name, flags) {
+    const { skipCurrentTab } = flags;
+
+    let result = [];
+
+    let foundCurrent = false;
+    for (const tab of tabs) {
+        if (name === SC_BEFORE_CURRENT_TAB) {
+            if (tab.active && skipCurrentTab) {
+                foundActive = true;
+                break;
+            }
+            result.push(tab);
+        } else if (name === SC_AFTER_CURRENT_TAB) {
+            if (tab.active) {
+                foundActive = true;
+            }
+            if (!foundActive) {
+                continue;
+            }
+            if (skipCurrentTab) {
+                continue;
+            }
+            result.push(tab);
+        } else if (name === SC_ATTENTION) {
+            if (!tab.attention) {
+                continue;
+            }
+            result.push(tab);
+        } else if (name === SC_ARTICLE) {
+            if (!tab.isArticle) {
+                continue;
+            }
+            result.push(tab);
+        } else if (name === SC_READER) {
+            if (!tab.isInReaderMode) {
+                continue;
+            }
+            result.push(tab);
+        }
+    }
+
+    return result;
+}
+
 async function messageHandler(msg) {
     if (msg.command !== "reload") return;
 
     try {
+        const { flags } = msg;
+        const { scenario } = flags;
         const queryParams = {};
 
         if (!flags.allNormalWins) {
             queryParams["currentWindow"] = true;
         }
+        handleScenario(queryParams, scenario);
 
-        const tabs = await bt.query(queryParams);
-        const { flags } = msg;
+        let tabs = await bt.query(queryParams);
+        tabs = filterTabsByScenario(tabs, scenario, flags);
+
         if (flags.orderReload) {
             for (const loadPromise of reloadTabs(tabs, flags)) {
                 await loadPromise;
             }
         } else {
-            await reloadTabs(tabs, msg.flags);
+            await reloadTabs(tabs, flags);
         }
     } catch (err) {
         console.error(jsonifyError(err));
