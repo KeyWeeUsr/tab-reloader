106a3070341da886efc0a32ad1ad1ccba7bf460f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2022-01-09 12:55:37 +0100

    [ADD] Add default values for new inputs

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 function normalizeFormData() {
     const defaults = {
-        "delaySec": 0, "noCache": false, "orderReload": false,
-        "waitForLoad": false
+        "allNormalWins": false, "delaySec": 0, "noCache": false,
+        "orderReload": false, "scenario": "all", "skipCurrentTab": false,
+        "skipCurrentWin": false, "waitForLoad": false, "withPrivate": false
     };
     const result = {};
     for (const [key, value] of new FormData(event.target)) {
